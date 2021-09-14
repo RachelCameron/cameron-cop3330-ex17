1e868c7cb1398f249a1adddcef5945679ca46983
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 20:45:32 -0400

    Commit

--- src/main/java/exercise17.java
+++ src/main/java/exercise17.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class exercise17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double bac = 0;
+        double ratio;
         System.out.println("Enter a 1 is you are male or a 2 if you are female:");
         int sex = input.nextInt();
         System.out.println("How many ounces of alcohol did you have?");
@@ -17,11 +17,14 @@ public class exercise17 {
         int pounds = input.nextInt();
         System.out.println("How many hours has it been since your last drink?");
         int hours = input.nextInt();
-        if(sex == 1)
-            bac = (ounces * 5.14 / pounds * .73) - .15 * hours;
-        else if(sex == 2)
-            bac = (ounces * 5.14 / pounds * .66) - .15 * hours;
-        System.out.printf("Your BAC is %f\n ", bac);
+        if(sex == 1) {
+            ratio = 0.73;
+        }
+        else {
+            ratio = 0.66;
+        }
+        double bac = (double)((ounces * (5.14 / pounds)*ratio) - .015 * hours);
+        System.out.printf("Your BAC is %f\n", bac);
         if(bac <= 0.08)
         {
             System.out.println("It is legal for you to drive.");
